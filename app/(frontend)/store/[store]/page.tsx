585588360e4f91d1a79fec897a0766c6dc6a6251
aaleccoder <alejandroperezacosta105+git@gmail.com>
2026-01-29 15:18:00 -0500

    fix: better auth authentication

--- app/(frontend)/store/[store]/page.tsx
+++ app/(frontend)/store/[store]/page.tsx
@@ -39,6 +39,8 @@ export default async function HomePage({
     notFound();
   }
 
+  console.log(storeSlug);
+
   const filterContent = await getFilterContent(
     storeSlug,
     store.id,
---
+++ prisma/migrations/20260129201615_added_is_paid_to_user/migration.sql
@@ -0,0 +1,11 @@
+-- AlterTable
+ALTER TABLE "user" ADD COLUMN     "isPaid" BOOLEAN DEFAULT false;
+
+-- CreateIndex
+CREATE INDEX "account_userId_idx" ON "account"("userId");
+
+-- CreateIndex
+CREATE INDEX "session_userId_idx" ON "session"("userId");
+
+-- CreateIndex
+CREATE INDEX "verification_identifier_idx" ON "verification"("identifier");
